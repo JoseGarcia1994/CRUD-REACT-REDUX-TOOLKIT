54357aa013ef29b3fe60804894a0ff49beab95fa
Jose Garcia <garciacolin@hotmail.com>
2023-11-16 17:15:32 -0600

    React-Router-Dom Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import './App.css'
+import './App.css';
+import {
+  HashRouter,
+  Routes,
+  Route
+} from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </HashRouter>
   )
 }
 
