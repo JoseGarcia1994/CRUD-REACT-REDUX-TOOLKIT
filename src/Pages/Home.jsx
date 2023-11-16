8212341a99b26d2df1aae788e79f270afb823beb
Jose Garcia <garciacolin@hotmail.com>
2023-11-16 17:10:08 -0600

    Home done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,6 +5,7 @@ const Home = () => {
     console.log(users);
     return (
         <div className='container'>
+            <h2>CRUD</h2>
             <button className='btn btn-success my-3'>Create +</button>
             <table className='table'>
                 <thead>
@@ -17,7 +18,17 @@ const Home = () => {
                 </thead>
 
                 <tbody>
-
+                    {users.map(user => (
+                        <tr key={user.id}>
+                            <td>{user.id}</td>
+                            <td>{user.name}</td>
+                            <td>{user.email}</td>
+                            <td>
+                                <button className="btn btn-sm btn-primary">Edit</button>
+                                <button className="btn btn-sm btn-danger ms-2">Delete</button>
+                            </td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </div>
